11eccd0813fa25270f4e50c572523a1eeb31b462
masa-mu <kkitune0911@gmail.com>
2019-06-28 10:13:18 +0900

    一旦削除

--- api-ddd-model/mysql/init.d/message.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `message`;
-
-CREATE TABLE message (
-  ID int NOT NULL PRIMARY KEY,
-  Message varchar(1024) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT message (ID,Message) VALUES (12345,"test");
